88b57e579d83cb61b6da8083398bff31a4d4f801
Rodrigo Barbosa <rodrigobarbosa@ypcontrol.com>
2026-07-10 15:19:31 -0300

    first commit

---
+++ src/test/java/com/drigobarbosa/superapp/SuperappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drigobarbosa.superapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
