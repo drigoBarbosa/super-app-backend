a1075d4d067c760cf9a9761aa6080d8b3e8e8a38
drigoBarbosa <rodrigobs8157@gmail.com>
2026-07-20 16:59:50 -0300

    feat: Adicionei os atributos do CreateTransactionRequest e iniciei a implementação do endpoint para criação da transação

--- src/main/java/com/drigobarbosa/superapp/financial/controller/TransactionController.java
+++ src/main/java/com/drigobarbosa/superapp/financial/controller/TransactionController.java
@@ -1,7 +1,18 @@
 package com.drigobarbosa.superapp.financial.controller;
 
+import com.drigobarbosa.superapp.financial.domain.entity.Transaction;
+import com.drigobarbosa.superapp.financial.dto.transaction.request.CreateTransactionRequest;
+import com.drigobarbosa.superapp.financial.dto.transaction.response.CreateTransactionResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TransactionController {
+
+    @PostMapping
+    public CreateTransactionResponse createTransaction(@RequestBody CreateTransactionRequest request) {
+        CreateTransactionResponse response = new CreateTransactionResponse();
+        return response;
+    }
 }
--- src/main/java/com/drigobarbosa/superapp/financial/dto/transaction/request/CreateTransactionRequest.java
+++ src/main/java/com/drigobarbosa/superapp/financial/dto/transaction/request/CreateTransactionRequest.java
@@ -1,5 +1,8 @@
 package com.drigobarbosa.superapp.financial.dto.transaction.request;
 
+import com.drigobarbosa.superapp.financial.domain.entity.Account;
+import com.drigobarbosa.superapp.financial.domain.entity.Category;
+import com.drigobarbosa.superapp.financial.domain.entity.Subcategory;
 import com.drigobarbosa.superapp.financial.domain.enums.TransactionMethod;
 import com.drigobarbosa.superapp.financial.domain.enums.TransactionType;
 
@@ -8,6 +11,9 @@ import java.math.BigDecimal;
 public record CreateTransactionRequest(
         String description,
         BigDecimal amount,
+        Account account,
+        Category category,
+        Subcategory subcategory,
         TransactionType type,
         TransactionMethod method
 ) { }
